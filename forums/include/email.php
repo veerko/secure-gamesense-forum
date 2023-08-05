6177f9da97bd0983093ba4379ae036c872e6c9d2
st1koo <antonluomala2008@gmail.com>
2023-08-05 07:26:16 +0300

    updated the cr

--- forums/include/email.php
+++ forums/include/email.php
@@ -1,11 +1,10 @@
 <?php
 
-/**
- * Copyright (C) 2008-2012 FluxBB
- * based on code by Rickard Andersson copyright (C) 2002-2008 PunBB
- * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
- */
-
+// Copyright (c) 2023 st1koo <https://github.com/st1koo>
+ //
+ // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ // You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
 // Make sure no one attempts to run this script "directly"
 if (!defined('PUN'))
 	exit;
